c5923c697a20d0a0cbd5766c64ff35f1c7987708
Arsene Le <chauanh1402@gmail.com>
2023-02-24 00:08:57 +0700

    Hotfix

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -11,15 +11,14 @@ func TestSendEmailWithGmail(t *testing.T) {
 		t.Skip()
 	}
 
-
-	sender := NewGmailSender("TVF admin", "tvf.sad@gmail.com", "hxjccxravqupuyos")
+	sender := NewGmailSender("Admin", "1640.sad@gmail.com", "hxjccxravqupuyos")
 
 	subject := "A test email"
 	content := `
 	<h1>Hello world</h1>
 	<p>This is a test message from <a href="http://techschool.guru">Tech School</a></p>
 	`
-	to := []string{"nhanphan215099@gmail.com"}
+	to := []string{"yoakearsene@gmail.com"}
 	attachFiles := []string{"../README.md"}
 
 	err := sender.SendEmail(subject, content, to, nil, nil, attachFiles)

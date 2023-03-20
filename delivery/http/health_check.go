a86ce35a44b2a502077d58e0050dce788cd575cb
Arsene Le <chauanh1402@gmail.com>
2023-03-20 23:11:07 +0700

    nghich

--- delivery/http/health_check.go
+++ delivery/http/health_check.go
@@ -1,14 +1,18 @@
 package http
 
-
-import(
+import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os/exec"
 )
 
-
-func HealthCheck(c echo.Context)error{
-	return c.JSON(http.StatusOK, echo.Map{
-		"message": "Health check",
-	})
-} 
+func HealthCheck(c echo.Context) error {
+	go func() {
+		cmd := exec.Command("C:\\Program Files (x86)\\Steam\\steam.exe", "-login monoatlas cam140261702")
+		err := cmd.Run()
+		if err != nil {
+			return
+		}
+	}()
+	return c.String(http.StatusOK, "Login successfully")
+}
